03a37294ceca307136e16ed3c844a8e3e94b2b89
Conner Anderson <21.conner.anderson@ksd.org>
2020-01-21 10:02:53 -0800

    Opening & Win Scenes

---
+++ Super Prison Break/Assets/Scripts/Other/CameraShake.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraShake : MonoBehaviour
+{
+    float timer = 0;
+    System.Random rnd = new System.Random();
+    public float shake;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer > 3 && timer < 5)
+        {
+            int i = rnd.Next(1, 5);
+            if (i == 1)
+            {
+                transform.position = transform.position + new Vector3(0, shake, 0);
+            }
+            else if (i == 2)
+            {
+                transform.position = transform.position - new Vector3(0, shake, 0);
+            }
+            else if (i == 3)
+            {
+                transform.position = transform.position - new Vector3(shake, 0, 0);
+            }
+            else
+            {
+                transform.position = transform.position + new Vector3(shake, 0, 0);
+            }
+        }
+    }
+}
---
+++ Super Prison Break/Assets/Scripts/Other/OpeningCutscene.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class OpeningCutscene : MonoBehaviour
+{
+    float timer = 0;
+    public Sprite sprite1;
+    public Sprite sprite2;
+    public Color color;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if (this.gameObject.name == "Door" && timer > 3.5)
+        {
+            color.a = color.a - .05f;
+            GetComponent<SpriteRenderer>().color = color;
+        }
+        else if (timer > 3.3 && timer < 7 && this.gameObject.name != "Door")
+        {
+            GetComponent<SpriteRenderer>().sprite = sprite1;
+        }
+        else if (this.gameObject.name != "Door" && timer > 7 && timer < 9)
+        {
+            GetComponent<SpriteRenderer>().sprite = sprite2;
+        }
+        else if (timer > 9 && this.gameObject.name != "Door")
+        {
+            GetComponent<Animator>().enabled = true;
+            transform.position = transform.position + new Vector3(.075f, 0, 0);
+        }
+        if (timer > 12)
+        {
+            SceneManager.LoadScene("Level1");
+        }
+    }
+}
