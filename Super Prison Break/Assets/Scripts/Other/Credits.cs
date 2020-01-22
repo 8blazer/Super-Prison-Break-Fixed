7ef67d9d0fe6d99c12c35c7f5cb139063160cbc7
Conner Anderson <connerwithahat@gmail.com>
2020-01-21 23:53:33 -0800

    Credit Scroll Randomizer

--- Super Prison Break/Assets/Scripts/Other/Credits.cs
+++ Super Prison Break/Assets/Scripts/Other/Credits.cs
@@ -2,66 +2,27 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using System.Linq;
 public class Credits : MonoBehaviour
 {
     System.Random rnd = new System.Random();
-    int i = 0;
     public Text first;
     public Text second;
     public Text third;
+    List<string> nameOrder = new List<string>();
     // Start is called before the first frame update
     void Start()
     {
+        nameOrder.Add("Conner Anderson");
+        nameOrder.Add("Taran Zorn");
+        nameOrder.Add("Stone Eskelesen");
+        int i = rnd.Next(1, 4);
+        first.text = nameOrder[i - 1];
+        nameOrder.RemoveAt(i - 1);
         i = rnd.Next(1, 3);
-        if (i == 1)
-        {
-            PlayerPrefs.SetString("FirstName", "Conner Anderson");
-        }
-        else if (i == 2)
-        {
-            PlayerPrefs.SetString("FirstName", "Taran Zorn");
-        }
-        else
-        {
-            PlayerPrefs.SetString("FirstName", "Stone Eskelsen");
-        }
-        PlayerPrefs.SetString("SecondName", PlayerPrefs.GetString("FirstName"));
-        while (PlayerPrefs.GetString("SecondName") == PlayerPrefs.GetString("FirstName"))
-        {
-            i = rnd.Next(1, 3);
-            if (i == 1)
-            {
-                PlayerPrefs.SetString("SecondName", "Conner Anderson");
-            }
-            else if (i == 2)
-            {
-                PlayerPrefs.SetString("SecondName", "Taran Zorn");
-            }
-            else
-            {
-                PlayerPrefs.SetString("SecondName", "Stone Eskelsen");
-            }
-        }
-        PlayerPrefs.SetString("ThirdName", PlayerPrefs.GetString("FirstName"));
-        while (PlayerPrefs.GetString("ThirdName") == PlayerPrefs.GetString("FirstName") || PlayerPrefs.GetString("ThirdName") == PlayerPrefs.GetString("SecondName"))
-        {
-            i = rnd.Next(1, 3);
-            if (i == 1)
-            {
-                PlayerPrefs.SetString("ThirdName", "Conner Anderson");
-            }
-            else if (i == 2)
-            {
-                PlayerPrefs.SetString("ThirdName", "Taran Zorn");
-            }
-            else
-            {
-                PlayerPrefs.SetString("ThirdName", "Stone Eskelsen");
-            }
-        }
-        first.text = PlayerPrefs.GetString("FirstName");
-        second.text = PlayerPrefs.GetString("SecondName");
-        third.text = PlayerPrefs.GetString("ThirdName");
+        second.text = nameOrder[i - 1];
+        nameOrder.RemoveAt(i - 1);
+        third.text = nameOrder[0];
     }
 
     // Update is called once per frame
